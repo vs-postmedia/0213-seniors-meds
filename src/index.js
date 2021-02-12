ff4af61a4b5759d9323d63943df27b4b3f4a02fc
Nathan Griffiths <ngriffiths@postmedia.com>
2021-02-12 09:27:39 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// LIBS
-import 'intersection-observer';
-import scrollama from 'scrollama';
 
 // CSS
 import normalize from './css/normalize.css';
@@ -12,26 +9,6 @@ import css from './css/main.css';
 
 // JS
 const init = async () => {
-	// instantiate the scrollama
-	const scroller = scrollama();
-	const iframe = document.querySelector('.scrollyteller .chart > iframe');
-
-	// setup the instance, pass callback functions
-	scroller
-		.setup({
-			offset: 0.75,
-			step: '.step',
-		})
-		.onStepEnter(resp => {
-			// { element, index, direction }
-			iframe.src = iframe.src.replace(/#slide-.*/, '') + '#slide-' + resp.index;
-		})
-		.onStepExit(resp => {
-			// { element, index, direction }
-		});
-
-	// setup resize event
-	window.addEventListener("resize", scroller.resize);
 };
 
 init();
